60231445619231ddf708ef567b846fec73eeae78
misha <mykhailo.ivaniuk@gmail.com>
2024-09-29 12:58:25 -0700

    fix slow j in visual mode, add linting

--- nvim/lua/misha/remap.lua
+++ nvim/lua/misha/remap.lua
@@ -8,14 +8,11 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("i", "jk", "<Esc>")
-vim.keymap.set("v", "jk", "<Esc>")
+vim.keymap.set("i", "hj", "<Esc>")
+vim.keymap.set("v", "hj", "<Esc>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- moves selected block up/down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- appends line below to the cur line with
-vim.keymap.set("n", "J", "mzJ`z")
 
 -- d moves within file down like 1/3 of it
 -- u moves up - half-page jumping
@@ -45,7 +42,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
---
+++ nvim/lua/plugins/linting.lua
@@ -0,0 +1,39 @@
+return {
+	"mfussenegger/nvim-lint",
+	opts = {
+		-- Event to trigger linters
+		events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+		linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			rust = { "clippy" },
+		},
+	},
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			rust = { "clippy" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		-- Nvim-[Lint] [L]int
+		vim.keymap.set("n", "<leader>ll", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
+}
