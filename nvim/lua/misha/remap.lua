21d647183f66eeb7124a9eb47d38550967d3b57f
Misha <65302583+mykhailoivaniuk@users.noreply.github.com>
2025-05-28 22:34:22 -0700

    tmux sessionizer

--- nvim/lua/misha/remap.lua
+++ nvim/lua/misha/remap.lua
@@ -46,3 +46,6 @@ vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set("n", "<up>", '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set("n", "<down>", '<cmd>echo "Use j to move!!"<CR>')
+
+-- Tmux sessionizer for the win
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
